a40aed5301a83e12d641903303de34c06c8b2a15
Washington Borges <washington.rborges@gmail.com>
2020-09-06 14:05:55 -0300

    Frontend initial with yarn

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <strong>#Hahtagtoday</strong>
+      </header>
+    </div>
+  );
+}
+
+export default App;
